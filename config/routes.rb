19f025be582cb2b87db2335f0a110fb6e7b3edcd
kenta <kenta.kamijukkoku@xincere.jp>
2026-09-21 17:48:13 +0900

    add/categoryのルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root "home#index"
   resource :household, only: [ :new, :create, :show ] # 世帯の作成・表示
   resource :invitation, only: [ :new, :create ]        # 招待コードでの参加
+  resources :categories, except: [ :show ] # カテゴリの作成・編集・削除
 end
