d9e818b635ced3da9df62171b9468ed2429af4b8
kenta <kenta.kamijukkoku@xincere.jp>
2026-09-21 15:14:39 +0900

    add/userモデルにリレーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+
+  has_one :household_member, dependent: :destroy
+  has_one :household, through: :household_member
 end
