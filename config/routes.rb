18adcc1aa00c2845402b1b1ca3db908239dcc5ef
kenta <kenta.kamijukkoku@xincere.jp>
2026-09-21 16:23:12 +0900

    add/routingの追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
+  resource :household, only: [ :new, :create, :show ] do # 世帯の作成・表示
+  resource :household_members, only: [ :new, :create ] # 世帯メンバーの追加
+  end
 end
