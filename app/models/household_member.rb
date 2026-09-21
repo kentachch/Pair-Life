578ddf7c374be605f97cb69693e6411f58aafe00
kenta <kenta.kamijukkoku@xincere.jp>
2026-09-21 15:26:57 +0900

    fix/rubocop

--- app/models/household_member.rb
+++ app/models/household_member.rb
@@ -3,8 +3,8 @@ class HouseholdMember < ApplicationRecord
   belongs_to :user
 
   validates :user_id, uniqueness: true # ユーザーは1つの世帯にしか所属できない
-  validates :burden_ratio, presence: true, 
-    numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 } 
+  validates :burden_ratio, presence: true,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   # 負担の割合は0以上100以下の整数であることを検証する
   validate :household_not_full, on: :create # 世帯の人数が上限に達していないかを検証する
   # on: :create: 作成時だけチェックする
