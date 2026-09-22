4a8913b0eb47831f45cce121dbb4744315b25647
kenta <kenta.kamijukkoku@xincere.jp>
2026-09-22 10:22:26 +0900

    add/scopeの記述

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -12,6 +12,12 @@ class Expense < ApplicationRecord
   validate :payer_must_be_household_member
   validate :category_must_belong_to_household
 
+  scope :in_month, ->(date) { where(spent_on: date.all_month) }
+  # dateを引数として受け取って、その月の1日から末日までの範囲で絞り込むスコープを定義。
+
+  scope :recent, -> { order(spent_on: :desc, created_at: :desc) }
+  # 日付の新しい順。同じ日付なら後から登録したものを上にする
+
   private
 
   def payer_must_be_household_member
@@ -23,10 +29,4 @@ class Expense < ApplicationRecord
     return if category.nil? || household.nil?
     errors.add(:category, "はこの世帯のカテゴリーではありません。") unless category.household_id == household.id
   end
-
-  scope :in_month, ->(date) { where(spent_on: date.all_month) }
-  # dateを引数として受け取って、その月の1日から末日までの範囲で絞り込むスコープを定義。
-
-  scope :recent, -> { order(spent_on: :desc, created_at: :desc) }
-  # 日付の新しい順。同じ日付なら後から登録したものを上にする
 end
